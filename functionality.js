5e1d4150f7b7a407b571606828650d4051edb187
orion <munna.csehstu@gmail.com>
2024-09-23 12:11:03 +0600

    doantion input functionality 70% completed

--- functionality.js
+++ functionality.js
@@ -2,8 +2,50 @@
 document.getElementById('blog').addEventListener('click', function () {
     window.location.href = "blog.html";
 })
+function getBalanceById(id) {
+    return Number(document.getElementById(id).innerText);
+}
+function getValueByInputElement(element) {
+    return element.value;
+}
+// global variables
+let totalBalance = getBalanceById("balance");
+let noakhaliBalancec = getBalanceById("noakhali-balance");
+let feniBalancec = getBalanceById("feni-balance");
+let quotaBalancec = getBalanceById("quota-balance");
 
-// donation and history functionality
+
+let donationCards = document.querySelectorAll(".donation-card");
+let donationInputs = document.querySelectorAll(".donate-input");
+let donationBtns = document.querySelectorAll(".donate-btn");
+
+// donation functionality
+function donate(evt, idx) {
+    
+    // const donateBtn = document.querySelector()
+    if (evt.target === donationBtns[idx]) {
+        let inputBalance = Number(donationInputs[idx].value);
+        console.log(inputBalance);
+        if(!isNaN(inputBalance))
+        {
+            console.log("Donation Successful");
+        }
+        else{
+            alert("Donation is Failed");
+        }
+    }
+
+}
+
+for (let i = 0; i < donationCards.length; i++) {
+    donationCards[i].addEventListener('click', function (evt) {
+        donate(evt, i);
+    })
+}
+
+
+
+// donation and history toggle functionality
 let donationActive = true; // true means donation is now active button, false means history in now active btn
 function changeDonationHistoryStyle(str1, str2) {
     // currently clicked button 
@@ -12,17 +54,17 @@ function changeDonationHistoryStyle(str1, str2) {
     // currently non-clicked button 
     document.getElementById(str2).classList.remove("bg-btn-color");
     document.getElementById(str2).classList.add("bg-white");
-   
+
     let donationSection = document.getElementById("donation-section");
     let historySection = document.getElementById("history-section");
-     // now active btn
-     console.log('hi');
+    // now active btn
+    console.log('hi');
     if (str1 === "donation") {
         console.log('see donation');
         donationActive = true;
         donationSection.classList.remove("hidden");
         donationSection.classList.add("flex");
-        historySection.classList.remove('flex');
+        historySection.classList.remove('grid'); //flex
         historySection.classList.add('hidden');
         // donationSection.classList.add("hidden bg-green-400");
         // donationSection.classList.remove("flex");
@@ -31,7 +73,7 @@ function changeDonationHistoryStyle(str1, str2) {
         console.log('see history');
         donationActive = false;
         historySection.classList.remove("hidden");
-        historySection.classList.add("flex");
+        historySection.classList.add("grid"); // flex
         donationSection.classList.remove('flex');
         donationSection.classList.add('hidden');
         // historySection.classList.add("hidden bg-green-400");
